62ffac17d5a06da8c3210ef44ae922819a528e72
yoavweber <yoav.weber@code.berlin>
2021-08-31 19:09:52 +0200

    fix: adding antd style to example

--- example/index.tsx
+++ example/index.tsx
@@ -2,19 +2,28 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { WeeklyCalendar } from '../.';
+// import { WeeklyCalendar } from 'antd-weekly-calendar';
+
+import { Card, PageHeader } from 'antd';
+import './index.less';
 
 const App = () => {
   const event = {
     eventId: '12',
     startTime: new Date(),
     endTime: new Date(),
+    title: 'test',
   };
   return (
     <div>
-      <WeeklyCalendar
-        events={[event]}
-        onEventClick={event => console.log(event)}
-      />{' '}
+      {/* <Calendar /> */}
+      <Card>
+        <WeeklyCalendar
+          events={[event]}
+          weekends={true}
+          onEventClick={event => console.log(event)}
+        />{' '}
+      </Card>
     </div>
   );
 };
