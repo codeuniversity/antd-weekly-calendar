c903764a840ecee39bdb6837a41b8fde155aaa08
yoavweber <yoav.weber@code.berlin>
2021-09-01 16:43:29 +0200

    fixing month name break in a week with 2 months

--- src/components/CalendarHeader.tsx
+++ src/components/CalendarHeader.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button, Row, Col, Tag, Typography } from "antd";
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import React from 'react';
+import { Button, Row, Col, Tag, Typography } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import {
   addWeeks,
   startOfWeek,
@@ -8,11 +8,11 @@ import {
   getMonth,
   format,
   getWeek,
-} from "date-fns";
+} from 'date-fns';
 
-import DatePicker from "./DatePicker";
+import DatePicker from './DatePicker';
 
-import { CalendarHeaderProps } from "./types";
+import { CalendarHeaderProps } from './types';
 
 interface MonthNameProps {
   startWeek: Date;
@@ -23,16 +23,17 @@ const MonthName: React.FunctionComponent<MonthNameProps> = ({ startWeek }) => {
     const endOfWeekDate = endOfWeek(startWeek);
 
     if (getMonth(endOfWeekDate) == getMonth(startWeek)) {
-      return format(startWeek, "MMM");
+      return format(startWeek, 'MMM');
     }
 
-    return format(startWeek, "MMM") + "-" + format(endOfWeekDate, "MMM");
+    return format(startWeek, 'MMM') + '-' + format(endOfWeekDate, 'MMM');
   };
+
   return (
-    <div style={{ display: "flex", maxHeight: "25px" }}>
+    <div style={{ display: 'flex', maxHeight: '25px' }}>
       <Typography.Title
         level={5}
-        style={{ marginBottom: 0, marginRight: "10px" }}
+        style={{ marginBottom: 0, marginRight: '10px' }}
       >
         {getMonthName()}
       </Typography.Title>
@@ -47,40 +48,39 @@ export const CalendarHeader: React.FunctionComponent<CalendarHeaderProps> = ({
 }) => {
   return (
     <>
-      <Row justify="space-between" style={{ marginBottom: "20px" }}>
+      <Row justify="space-between" style={{ marginBottom: '20px' }}>
         <Col
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            maxWidth: "200px",
+            display: 'flex',
+            justifyContent: 'space-between',
           }}
         >
-          <div style={{ display: "flex", marginRight: "20px" }}>
+          <div style={{ display: 'flex', marginRight: '20px' }}>
             <Button onClick={() => setStartWeek(startOfWeek(new Date()))}>
               Today
             </Button>
-            <div style={{ display: "flex", padding: "0 10px" }}>
+            <div style={{ display: 'flex', padding: '0 10px' }}>
               <Button
-                style={{ margin: "0 5px" }}
+                style={{ margin: '0 5px' }}
                 onClick={() => setStartWeek(addWeeks(startWeek, -1))}
               >
                 <LeftOutlined />
               </Button>
               <Button
-                style={{ margin: "0 5px" }}
+                style={{ margin: '0 5px' }}
                 onClick={() => setStartWeek(addWeeks(startWeek, 1))}
               >
                 <RightOutlined />
               </Button>
             </div>
           </div>
-          <div style={{ alignSelf: "center" }}>
+          <div style={{ alignSelf: 'center' }}>
             <MonthName startWeek={startWeek} />
           </div>
         </Col>
         <Col>
           <DatePicker
-            onChange={(date) => {
+            onChange={date => {
               if (date) {
                 setStartWeek(startOfWeek(new Date(date)));
               }
