049cbbe3e96f0bbcb84db3894d96310d32e304ca
yoavweber <yoav.weber@code.berlin>
2024-08-19 23:59:20 +0300

    moving to vite and updating node version

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        lib: {
+            entry: 'src/index.tsx',
+            name: 'AntdWeeklyCalendar',
+            fileName: (format) => `antd-weekly-calendar.${format}.js`
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM'
+                }
+            }
+        }
+    }
+});
